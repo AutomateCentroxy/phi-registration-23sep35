639976137cd91e989ce497401ac8ff264020f22d
AutomateCentroxy <155064161+AutomateCentroxy@users.noreply.github.com>
2025-09-24 17:28:46 +0530

    refactor: update UserRegistration.java

--- projects/agama-newuser-registration/lib/org/gluu/agama/user/UserRegistration.java
+++ projects/agama-newuser-registration/lib/org/gluu/agama/user/UserRegistration.java
@@ -18,7 +18,7 @@ public abstract class UserRegistration {
 
     public abstract String sendEmail(String to);
     
-    public abstract String markPhoneAsVerified(String to);
+    public abstract String markPhoneAsVerified(String uid);
 
     public abstract boolean checkIfUserExists(String username, String email);
 
